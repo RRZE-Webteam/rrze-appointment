f58a9f86c1122061208f39b2a24fe604933b62e6
Benjamin Klemencic <benjamin.klemencic@fau.de>
2026-03-20 16:21:59 +0100

    0.1.0 (cal layout & functions: done, booking: .ics mailed)

--- rrze-appointment.php
+++ rrze-appointment.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        RRZE Appointment
 Plugin URI:         https://github.com/RRZE-Webteam/rrze-appointment
-Version:            0.0.63
+Version:            0.0.65
 Description:        Explain your content with FAQ, glossary and placeholders.
 Author:             RRZE Webteam
 Author URI:         https://www.wp.rrze.fau.de/
