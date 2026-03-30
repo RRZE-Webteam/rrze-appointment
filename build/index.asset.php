773d01cfbafdafe928e9a5ada702ad7246cd5cb1
Benjamin Klemencic <benjamin.klemencic@fau.de>
2026-03-30 14:16:00 +0200

    improved sass / split .css editor/frontend/preview

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'a7d52c3ff07bd096947c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '0647cfedf300e9541a32');
