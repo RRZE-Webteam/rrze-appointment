b86fd0445757996443a6e710fb3a4fe9bcc03ba1
Benjamin Klemencic <benjamin.klemencic@fau.de>
2026-03-30 10:09:28 +0200

    debugging SSO

--- src/view.js
+++ src/view.js
@@ -243,6 +243,8 @@
             button.textContent = slot.label;
             button.dataset.slotValue = slot.value;
 
+            // alert('createSlotButton');
+
             if (isBooked) {
                 button.classList.add('is-booked');
                 button.disabled = true;
@@ -254,6 +256,9 @@
             }
 
             button.addEventListener('click', () => {
+
+                alert('servas');
+
                 button.disabled = true;
                 const data = new FormData();
                 data.append('action', 'rrze_appointment_get_booker');
