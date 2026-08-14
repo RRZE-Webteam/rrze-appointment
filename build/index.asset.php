572d455686a74665926d22dfe7a0c7597960acf8
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-08-14 11:14:09 +0200

    fix(styling) - Adjust CSS
    Adjust styling of timeslot overview and border-top of the edit view.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'c7101163f5f42f211a03');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'd9cdbf689236a0ba2bce');
